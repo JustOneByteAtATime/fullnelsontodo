e1df27d5375b645f29abd142b7ccdfaf72a56998
Michael Nelson <michael1.nelson@umconnect.umt.edu>
2022-10-16 12:03:18 -0600

    "New Task Sheet Updates Functional"

--- app/src/main/java/com/example/fullnelsontodolist/MainActivity.kt
+++ app/src/main/java/com/example/fullnelsontodolist/MainActivity.kt
@@ -10,21 +10,14 @@ class MainActivity : AppCompatActivity()
     private lateinit var binding: ActivityMainBinding
     private lateinit var taskViewModel: TaskViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
         binding.newTaskButton.setOnClickListener {
-            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
-        }
-
-        taskViewModel.name.observe(this){
-            binding.taskName.text = String.format("Task Name: %s", it)
-        }
-
-        taskViewModel.desc.observe(this){
-            binding.taskDesc.text = String.format("Task Description: %s", it)
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
     }
 }
--- app/src/main/java/com/example/fullnelsontodolist/TaskViewModel.kt
+++ app/src/main/java/com/example/fullnelsontodolist/TaskViewModel.kt
@@ -2,8 +2,40 @@ package com.example.fullnelsontodolist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.*
 
 class TaskViewModel: ViewModel() {
-    var name = MutableLiveData<String>()
-    var desc = MutableLiveData<String>()
+    var taskItems = MutableLiveData<MutableList<TaskItem>>()
+
+    init{
+        taskItems.value = mutableListOf()
+    }
+
+    fun addTaskItem(newTask: TaskItem){
+        val list = taskItems.value
+        list!!.add(newTask)
+        taskItems.postValue(list)
+    }
+    //The double exclamation mark (!!) after list above, is used because we know the list is NOT null
+
+    fun updateTaskItem(id: UUID, name: String, desc: String, dueTime: LocalTime?)
+    {
+        val list = taskItems.value
+        val task = list!!.find { it.id == id }!!
+        task.name = name
+        task.desc = desc
+        task.dueTime = dueTime
+        taskItems.postValue(list)
+    }
+
+    fun setCompleted(taskItem: TaskItem){
+        val list = taskItems.value
+        val task = list!!.find { it.id == taskItem.id }!!
+        if (task.completedDate == null)
+            task.completedDate = LocalDate.now()
+        taskItems.postValue(list)
+    }
+
 }
